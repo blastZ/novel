a3a095afbeb80715d1fe6790e58c84f39cf23f26
blastz <touchzh1996@gmail.com>
2019-09-02 20:25:01 +0800

    Update

--- server/src/graphql/controllers/book/biqiuge/get.js
+++ server/src/graphql/controllers/book/biqiuge/get.js
@@ -3,7 +3,7 @@ const iconv = require('iconv-lite');
 const cheerio = require('cheerio');
 const path = require('path');
 
-const URL = 'https://www.biqiuge.com';
+const URL = 'https://www.biqiuge8.com';
 
 module.exports = async id => {
   const url = encodeURI(`${URL}/book/${id}/`);
@@ -15,6 +15,21 @@ module.exports = async id => {
   const $ = cheerio.load(html);
 
   const name = $('body > div.book > div.info > h2').text();
+  const category = $('body > div.book > div.info > div.small > span:nth-child(2)')
+    .text()
+    .split('：')[1]
+    .trim(0);
+  const status = $('body > div.book > div.info > div.small > span:nth-child(3)')
+    .text()
+    .split('：')[1]
+    .trim();
+  const thumb = URL + $('body > div.book > div.info > div.cover > img').attr('src');
+  const desc = $('body > div.book > div.info > div.intro')
+    .text()
+    .split('：')[1]
+    .trim()
+    .slice(0, -2)
+    .replace(/[\t\n\d\s]/g, '');
   const author = $('body > div.book > div.info > div.small > span:nth-child(1)')
     .text()
     .split('：')[1]
@@ -23,24 +38,16 @@ module.exports = async id => {
     .text()
     .split('：')[1]
     .trim();
-  const status = $('body > div.book > div.info > div.small > span:nth-child(3)')
-    .text()
-    .split('：')[1]
-    .trim();
   const latestA = $('body > div.book > div.info > div.small > span:nth-child(6) > a');
   const latest = {
     id: path.basename(latestA.attr('href')).split('.')[0],
     name: latestA.text()
   };
-  const desc = $('body > div.book > div.info > div.intro')
-    .text()
-    .split('：')[1]
-    .trim();
-  const thumb = URL + $('body > div.book > div.info > div.cover > img').attr('src');
 
   return {
     id,
     name,
+    category,
     status,
     thumb,
     desc,
